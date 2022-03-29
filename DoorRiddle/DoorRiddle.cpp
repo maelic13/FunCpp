f846727216c207663b0487909f9c457a30771eaa
Miloslav Macůrek <miloslav.macurek@thermofisher.com>
2022-03-29 09:11:52 +0200

    Code style

--- DoorRiddle/DoorRiddle.cpp
+++ DoorRiddle/DoorRiddle.cpp
@@ -6,13 +6,12 @@ using namespace std;
 
 unsigned long xorshf96(void) {
     static unsigned long x = 123456789, y = 362436069, z = 521288629;
-    unsigned long t;
 
     x ^= x << 16;
     x ^= x >> 5;
     x ^= x << 1;
 
-    t = x;
+    const unsigned long t = x;
     x = y;
     y = z;
     z = t ^ x ^ y;
@@ -21,12 +20,12 @@ unsigned long xorshf96(void) {
 }
 
 
-long long PlayGame(long long cycles, bool changeChoice) {
-    bool doors[] = { false, true, false };
+long long play_game(const long long cycles, const bool change_choice) {
+    constexpr bool doors[] = { false, true, false };
     long long successful = 0;
 
     for (int i{}; i < cycles; i++) {
-        if (changeChoice != doors[xorshf96()]) {
+        if (change_choice != doors[xorshf96()]) {
             successful += 1;
         }
     }
@@ -35,28 +34,27 @@ long long PlayGame(long long cycles, bool changeChoice) {
 }
 
 
-void print_results(long long successful, long long cycles, bool changeChoice, std::chrono::high_resolution_clock::time_point start) {
-    typedef std::chrono::high_resolution_clock Clock;
-    typedef std::chrono::duration<double> sec;
-
-    auto elapsed = sec(Clock::now() - start).count();
+void print_results(const long long successful, const long long cycles, const bool change_choice, const double elapsed) {
     cout << endl;
-    cout << "Change = " << changeChoice << ". Time elapsed: " << elapsed * 1000 << " ms." << endl;
-    cout << successful << " successful tries, " << cycles << " total. Success rate " << static_cast<double>(successful) / cycles * 100. << " %." << endl;
+    cout << "Change = " << change_choice << ". Time elapsed: " << elapsed * 1000 << " ms." << endl;
+    cout << successful << " successful tries, " << cycles << " total. Success rate " << static_cast<double>(successful)
+        / cycles * 100. << " %." << endl;
     cout << "Speed = " << cycles / elapsed / 1000000 << " Miter/s." << endl;
 }
 
 
 int main()
 {
-    auto cycles = 1000000000;
+    constexpr auto cycles = 1000000000;
 
     typedef std::chrono::high_resolution_clock Clock;
+    typedef std::chrono::duration<double> sec;
+
     Clock::time_point start = Clock::now();
-    auto successful = PlayGame(cycles, false);
-    print_results(successful, cycles, false, start);
+    auto successful = play_game(cycles, false);
+    print_results(successful, cycles, false, sec(Clock::now() - start).count());
 
     start = Clock::now();
-    successful = PlayGame(cycles, true);
-    print_results(successful, cycles, true, start);
+    successful = play_game(cycles, true);
+    print_results(successful, cycles, true, sec(Clock::now() - start).count());
 }
